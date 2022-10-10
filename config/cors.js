0d8f91d66e896b093a444e139382ca4c4269c36c
AndyOoh <andy.oeee@gmail.com>
2022-10-10 16:54:29 +0700

    cors

--- config/cors.js
+++ config/cors.js
@@ -2,18 +2,18 @@ console.log('inside app-config');
 import { FRONTEND_URL } from './VARS.js';
 
 export const corsConfig = {
-  origin: [FRONTEND_URL, 'http://localhost:3001'],
+  origin: [FRONTEND_URL, 'http://localhost:3001', 'https://www.theredbook.xyz/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], //notice OPTIONS not needed for preflight req to pass
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Origin',
-    'Content-Type',
-    'Authorization',
-    'Content-length',
-    'Access-Control-Allow-Headers',
+  // allowedHeaders: [
+  //   'Access-Control-Allow-Origin',
+  //   'Origin',
+  //   'Content-Type',
+  //   'Authorization',
+  //   'Content-length',
+  //   'Access-Control-Allow-Headers',
 
-    'X-Requested-With', //not sure what it is. came from SO
-    'Accept', //not sure what it is. came from SO
-  ],
+  //   'X-Requested-With', //not sure what it is. came from SO
+  //   'Accept', //not sure what it is. came from SO
+  // ],
 };
